fb7893dd5c043540afadf953d8f84bf4d3583d7d
Geoffrey CARPENTIER <geoffrey.carpentier@laplateforme.io>
2025-12-04 16:24:25 +0100

    Job05

--- jour01/job05/index.php
+++ jour01/job05/index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jour 01 - Job 05</title>
+    <script src="script.js"></script>
+
 </head>
 <body>
-    
+    <!-- ... -->
 </body>
 </html>
--- jour01/job05/script.js
+++ jour01/job05/script.js
@@ -1,4 +1,10 @@
-
-
+function afficherjourssemaines() {  // La fonction ne prend pas de paramètre
+    // let permet 
+    let jourssemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let i = 0; i < jourssemaines.length; i++) {
+        console.log(jourssemaines[i]);
+    }
+}
+afficherjourssemaines();  // Appel de la fonction pour afficher les jours
 // Pour test dans terminal VSCode, taper:
 //" ...\www\runtrack3\jour01\job05> node script.js"
