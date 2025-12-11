3c88e570df58bb625254ca94cbe247311cbbc3a2
Geoffrey CARPENTIER <geoffrey.carpentier@laplateforme.io>
2025-12-11 14:46:58 +0100

    Job02: Ajout du script.js avec la fonction 'jsonValueKey' qui extrait des valeurs d'une chaîne JSON + exemples d'utilisation (+ 'script+.js'...)

--- jour04/job02/script+.js
+++ jour04/job02/script+.js
@@ -1,8 +1,32 @@
-function jsonValueKey(jsonString, key) {
+function jsonValueKey(jsonString, key) {  // la fonction prend une chaine de caractère "jsonString" et une clé "key"
     try {
         const jsonObj = JSON.parse(jsonString);
         return jsonObj[key];
     } catch (error) {
         console.error("Invalid JSON string:", error);
         return null;
-    }
+    } // convertir la chaîne JSON en objet et retourner la valeur associée à la clé
+}
+
+//* Exemple d'utilisation:
+
+console.log(jsonValueKey('{"name":"La Plateforme_","address":"8 rue d\'hozier","city":"Marseille","nb_staff":"11","creation":"2019"}', 'city')); 
+
+//* Exemples d'appels de la fonction avec différentes clés (différents cas de figure)
+
+console.log(jsonValueKey('{"name":"La Plateforme_","address":"8 rue d\'hozier","city":"Marseille","nb_staff":"11","creation":"2019"}', 'nb_staff' , "name")); 
+//? Ici la fonction ne retourne que la valeur de la première clé car les paramètres après la première clé sont ignorés
+
+console.log(jsonValueKey('{"name":"La Plateforme_","address":"8 rue d\'hozier","city":"Marseille","nb_staff":"11","creation":"2019"}', "address" || "name")); 
+//? Ici encore la fonction ne retourne que la valeur de la première clé car "||" est un opérateur logique, la clé "name" n'est pas évaluée 
+
+console.log(jsonValueKey('{"name":"La Plateforme_","address":"8 rue d\'hozier","city":"Marseille","nb_staff":"11","creation":"2019"}', "address" && "name")); 
+//? Ici c'est la valeur de la dernière clé qui est retournée car "&&" est un opérateur logique, c'est la clé "address" qui n'est pas évaluée   WHY???
+
+// Pour obtenir plusieurs valeurs il faut appeler la fonction plusieurs fois avec des clés différentes
+console.log(jsonValueKey('{"name":"La Plateforme_","address":"8 rue d\'hozier","city":"Marseille","nb_staff":"11","creation":"2019"}', 'address')); 
+console.log(jsonValueKey('{"name":"La Plateforme_","address":"8 rue d\'hozier","city":"Marseille","nb_staff":"11","creation":"2019"}', 'name'));
+
+
+
+ 
