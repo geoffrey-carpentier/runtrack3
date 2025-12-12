c3ed5dd805a2c59f260c30a356cd10a299d64ac4
Geoffrey CARPENTIER <geoffrey.carpentier@laplateforme.io>
2025-12-12 17:01:01 +0100

    Ajout des formulaires de connexion et d'inscription pour le jour 05, avec validation asynchrone et gestion des erreurs.

---
+++ jour05/job00/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 05 - Projet</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1>Jour 05: Projet</h1>
+        <p>Que la force soit avec toi …</p>
+    </div>
+
+    <div class="container">
+        <!-- Introduction du sujet -->
+        <h2>Introduction du sujet</h2>
+        <p>
+            Dans le répertoire de rendu <span class="highlight">"runtra ckJs"</span>, créez un dossier <span class="highlight">"jour05"</span>.
+        </p>
+        <p>
+            Pour chaque job, créez un dossier <span class="highlight">"jobXX"</span> où XX est le numéro du job.
+            N'oubliez pas d'envoyer vos modifications dès qu'un job est avancé ou terminé et mettez des commentaires explicites.
+        </p>
+
+        <!-- Job 01 -->
+        <h2><a href="../job01/">Job 01</a></h2>
+        <p>Commencez par créer deux fichiers HTML <span class="highlight">"connexion.html"</span> et <span class="highlight">"inscription.html"</span> contenant un formulaire.</p>
+
+        <h3>Le formulaire de la page de connexion doit contenir les champs suivants :</h3>
+        <ul>
+            <li>Email</li>
+            <li>Mot de passe</li>
+        </ul>
+
+        <h3>Le formulaire de la page inscription être composé au minimum des champs suivants :</h3>
+        <ul>
+            <li>Nom</li>
+            <li>Prénom</li>
+            <li>Email</li>
+            <li>Mot de passe</li>
+            <li>Adresse</li>
+            <li>Code postal</li>
+        </ul>
+
+        <p>
+            Faites de la veille sur les différentes sécurités à mettre sur un formulaire côté front.
+            Chaque champ doit être vérifié de façon <span class="highlight">asynchrone</span>.
+        </p>
+
+        <p>
+            Un message lié à chaque erreur doit être affiché en dessous de chaque champ du formulaire le cas échéant.
+        </p>
+
+        <!-- Job 02 -->
+        <h2><a href="../job02/">Job 02</a></h2>
+        <p>Avez-vous fini les jobs des jours précédents ?</p>
+
+        <!-- Compétences visées -->
+        <h2>Compétences visées</h2>
+        <ul>
+            <li>Gérer les vérifications de formulaire en temps réel</li>
+        </ul>
+
+        <!-- Rendu -->
+        <h2>Rendu</h2>
+        <p>
+            Le projet est à rendre sur <a href="https://github.com/geoffrey-carpentier/runtrack3" target="_blank">https://github.com/prenom-nom/runtrack3</a>.
+            Pensez à mettre votre repository en public !
+        </p>
+    </div>
+</body>
+
+</html>
